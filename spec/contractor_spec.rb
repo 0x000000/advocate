997fbbec74c07bf6fc5ab5f660b05d325b311955
Alexander Rudenko <mur.mailbox@gmail.com>
2013-09-16 13:46:57 +0300

    Specs for contractor

--- spec/contractor_spec.rb
+++ spec/contractor_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Advocate::Contractor do
+  context 'when included to some class' do
+    class AdvContractorTestClass
+      include Advocate::Contractor
+    end
 
+    it 'should add contract_for class method' do
+      AdvContractorTestClass.methods.should include(:contract_for)
+    end
+  end
 end
